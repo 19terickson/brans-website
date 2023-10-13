ec3f4aed8b2a575575bac5caa71adf0cbb8f22ad
19terickson <traceerickson@mail.weber.edu>
2023-10-13 01:38:28 -0600

    made minor tweaks to the site

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -5,18 +5,27 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "../stylesheets/main.scss"
+import Navbar from "./navbar";
+import Footer from "./footer"
 
 export default function Home() {
     const [user, setUser] = useState({});
     const navigate = useNavigate();
 
     return (
+        <body>
+       {/* navbar */}
+       <Navbar />
+       {/* title */}
         <div className="home-hero text-center">
             <div className="background">
                 <h1>Bransen D. Erickson</h1>
-                <h3>All About Me</h3>
-                <a className="home-button" href= "/about">About Me</a>
+                <a className="home-button" href= "/about">Enter</a>
             </div>
         </div>
+            {/* footer */}
+            <Footer class="Footer"/>
+            {/* title */}
+        </body>
     );
 }
